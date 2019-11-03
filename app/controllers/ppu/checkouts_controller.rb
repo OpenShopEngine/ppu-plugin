ca152bcb48443ec501be24d5b9c7e5afe7daa2d2
Pavel Kosilo <kodosgames@gmail.com>
2019-11-03 22:52:11 +0300

    Editing controllers to check is user: admin or payment_system

--- app/controllers/ppu/checkouts_controller.rb
+++ app/controllers/ppu/checkouts_controller.rb
@@ -3,36 +3,56 @@ require_dependency "ppu/application_controller"
 module Ppu
   class CheckoutsController < ApplicationController
     before_action :set_checkout, only: [:show, :destroy]
+    before_action :current_user, only: [:index, :show, :create, :destroy]
 
     # GET /checkouts
     def index
-      @checkouts = Checkout.all
-
-      render json: @checkouts
+      if @current_user.role? :admin
+        @checkouts = Checkout.all
+        render json: @checkouts
+      else
+        render json: "Only for admins!", status: :bad_request
+      end
     end
 
     # GET /checkouts/1
     def show
-      render json: @checkout
+      if @current_user.role? :admin
+        render json: @checkout
+      else
+        render json: "Only for admins!", status: :bad_request
+      end
     end
 
     # POST /checkouts
     def create
-      @checkout = Checkout.new(ActiveSupport::JSON.decode(request.body.read))
+      if @current_user.role? :payment_system
+        @checkout = Checkout.new(ActiveSupport::JSON.decode(request.body.read))
 
-      if @checkout.save
-        render json: @checkout, status: :created, location: @checkout
+        if @checkout.save
+          render json: @checkout, status: :created, location: @checkout
+        else
+          render json: @checkout.errors, status: :unprocessable_entity
+        end
       else
-        render json: @checkout.errors, status: :unprocessable_entity
+        render json: "NOT_PAYMENT_SYSTEM", status: :bad_request
       end
     end
 
     # DELETE /checkouts/1
     def destroy
-      @checkout.destroy
+      if @current_user.role? :admin
+        @checkout.destroy
+      else
+        render json: "Only for admins!", status: :bad_request
+      end
     end
 
     private
+      def current_user
+        @current_user = User.find(session[:user_id])
+      end
+
       # Use callbacks to share common setup or constraints between actions.
       def set_checkout
         @checkout = Checkout.find(params[:id])
--- app/controllers/ppu/transactions_controller.rb
+++ app/controllers/ppu/transactions_controller.rb
@@ -3,36 +3,56 @@ require_dependency "ppu/application_controller"
 module Ppu
   class TransactionsController < ApplicationController
     before_action :set_transaction, only: [:show, :destroy]
+    before_action :current_user, only: [:index, :show, :create, :destroy]
 
     # GET /transactions
     def index
-      @transactions = Transaction.all
-
-      render json: @transactions
+      if @current_user.role? :admin
+        @transactions = Transaction.all
+        render json: @transactions
+      else
+        render json: "Only for admins!", status: :bad_request
+      end
     end
 
     # GET /transactions/1
     def show
-      render json: @transaction
+      if @current_user.role? :admin
+        render json: @transaction
+      else
+        render json: "Only for admins!", status: :bad_request
+      end
     end
 
     # POST /transactions
     def create
-      @transaction = Transaction.new(ActiveSupport::JSON.decode(request.body.read))
+      if @current_user.role? :payment_system
+        @transaction = Transaction.new(ActiveSupport::JSON.decode(request.body.read))
 
-      if @transaction.save
-        render json: @transaction, status: :created, location: @transaction
+        if @transaction.save
+          render json: @transaction, status: :created, location: @transaction
+        else
+          render json: @transaction.errors, status: :unprocessable_entity
+        end
       else
-        render json: @transaction.errors, status: :unprocessable_entity
+        render json: "NOT_PAYMENT_SYSTEM", status: :bad_request
       end
     end
 
     # DELETE /transactions/1
     def destroy
-      @transaction.destroy
+      if @current_user.role? :admin
+        @transaction.destroy
+      else
+        render json: "Only for admins!", status: :bad_request
+      end
     end
 
     private
+      def current_user
+        @current_user = User.find(session[:user_id])
+      end
+
       # Use callbacks to share common setup or constraints between actions.
       def set_transaction
         @transaction = Transaction.find(params[:id])
